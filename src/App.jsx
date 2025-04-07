411269e0909f503b024e5aec0befc4409f9f9d35
Viktoriia Sherstobitova <vikusher@gmail.com>
2025-04-07 18:58:16 -0400

    changes frim week 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import './App.css'
-import TodoList from './TodoList'
-import TodoForm from './TodoForm'
+import './App.css';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+import { useState } from 'react';
+
 
 function App() {
 
+  const [newTodo, setNewTodo] = useState('My first todo:');
+
   return (
     <div>
-      <h1>My Todos</h1>
-      <TodoForm />
+      <h1>Todo List</h1>
+      <TodoForm setNewTodo={setNewTodo} />
+      <p>{newTodo}</p>
       <TodoList /> 
     </div>
   )
 }
 
-export default App
+export default App;
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,11 +1,11 @@
 function TodoForm(){
-    return (
-        <form>
-          <label htmlFor="todoTitle"> Todo </label>
-          <input type="text" id="todoTitle" />
-          <button type="submit"> Add Todo </button>
-        </form>
-      );
+  return (
+      <form>
+        <label htmlFor="todoTitle"> Todo </label>
+        <input type="text" id="todoTitle" />
+        <button type="submit"> Add Todo </button>
+      </form>
+    );
 }
 
-export default TodoForm
+export default TodoForm;
